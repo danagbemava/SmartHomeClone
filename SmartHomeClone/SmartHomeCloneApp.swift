ea7ab669329a4e3ca401c4d2985a5355b6a9b83c
danagbemava-nc <daniel@nevercode.io>
2022-07-20 19:50:01 +0000

    Initial Commit

---
+++ SmartHomeClone/SmartHomeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeCloneApp.swift
+//  SmartHomeClone
+//
+//  Created by Daniel Agbemava on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
